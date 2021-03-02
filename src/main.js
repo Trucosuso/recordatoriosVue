ad9a5ff0da87a819f817d58ade75cc3f5317b435
Jesús L. Pinzolas <jesus@trucosuso.com>
2021-03-02 01:00:39 +0100

    Added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from "vue"
 import App from "./App.vue"
 
+// Add bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+// Use bootstrap and bootstrap icons
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+
 Vue.config.productionTip = false
 
 // eslint-disable-next-line vue/require-name-property
